8c5d682267b247aa463b3d6a687f74268de41979
MayaS111 <mayasheriff2018@gmail.com>
2024-06-20 21:58:34 -0500

    Done!

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
   
   ## Direct associations
   # User#comments: returns rows from the comments table associated to this user by the author_id column
-  has_many(:comments, class_name: "Comment", foreign_key: "actor_id")
+  has_many(:comments, class_name: "Comment", foreign_key: "author_id")
 
   # User#own_photos: returns rows from the photos table  associated to this user by the owner_id column
   has_many(:own_photos, class_name: "Photo", foreign_key: "owner_id")
@@ -33,15 +33,15 @@ class User < ApplicationRecord
   has_many(:sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id")
 
   # User#received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column
-  has_many(:received_follow_request, class_name: "FollowRequest", foreign_key: "recipient_id")
+  has_many(:received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id")
 
 
   ### Scoped direct associations
   # User#accepted_sent_follow_requests: returns rows from the follow requests table associated to this user by the sender_id column, where status is 'accepted'
-  has_many(:accepted_sent_follow_requests, -> {where status: "accept"}, class_name: "FollowRequest", foreign_key: "sender_id")
+  has_many(:accepted_sent_follow_requests, -> {where status: "accepted"}, class_name: "FollowRequest", foreign_key: "sender_id")
 
   # User#accepted_received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column, where status is 'accepted'
-  has_many(:accepted_received_follow_requests, -> {where status: "accept"}, class_name: "FollowRequest", foreign_key: "recipient_id")
+  has_many(:accepted_received_follow_requests, -> {where status: "accepted"}, class_name: "FollowRequest", foreign_key: "recipient_id")
 
 
   ## Indirect associations
